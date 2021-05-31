354c14b7913e384c80b3033080595a68ee57dad7
Alexander Zabiyako <szabiyako@gmail.com>
2021-05-31 10:51:23 +0300

    Added Texture data fileds for Default and PBR lighting methods

--- LightExamples/src/LoadableData/LoadableData.h
+++ LightExamples/src/LoadableData/LoadableData.h
@@ -1,11 +1,27 @@
 #pragma once
 
 #include "ObjData/ObjData.h"
+#include "Image/Image.h"
 
 struct LoadableData
 {
+	struct Default {
+		Image diffuse;
+		Image specular;
+		Image normal;
+	};
+	struct PBR {
+		Image albedo;
+		Image metallic;
+		Image roughness;
+		Image normal;
+		Image ao;
+	};
+
 	ObjData objData;
-	
+	Default defaultData;
+	PBR pbrData;
+
 
 	LoadableData();
 	LoadableData(const ObjData &objData);
